2ed6b3f726b4bd299c08d74761d32498c169cbb2
jetpotion <William.Zhang@stonybrook.edu>
2018-12-31 12:02:22 -0500

    Dkistra updated,MST goals updated,

---
+++ GreedyAlgorithms/src/Greedyalgorithms/DkistraShortestPath.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Greedyalgorithms;
+
+/**
+ *
+ * @author William Zhang
+ */
+import DataStructures.FibonacciHeap;
+
+public class DkistraShortestPath {
+  /** To be done later but a fibonacci heap is required for future reference. This will speed up run time from O(Vlog(E)) to O(E + Vlog(v)) **/
+   /** The motivation behind this designed with a fibonacci heap is prevent the cardinality of the edges **/
+public  static void main(String []args)
+{
+    
+}
+public static void ShortTestPathAlgorithm()
+{
+    
+}
+}
--- GreedyAlgorithms/src/Greedyalgorithms/MST.java
+++ GreedyAlgorithms/src/Greedyalgorithms/MST.java
@@ -10,5 +10,6 @@ package Greedyalgorithms;
  * @author William Zhang
  */
 public class MST {
-    
+/** Implement some form  Delauney Triangulation to speed up runtime. **/
+ /**Delauney triangulation will snip the tree until it is planar  and thus limit  the number of edges to be less than 3v-6 where V is the number of edges **/
 }
